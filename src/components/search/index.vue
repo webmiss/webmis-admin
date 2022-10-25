142df9e67dd14f7be97765b97e3d863c8ce40bac
webmis <klingsoul@163.com>
2022-10-25 11:13:04 +0800

    UI

--- src/components/search/index.vue
+++ src/components/search/index.vue
@@ -17,7 +17,7 @@
 
 <style lang="less">
 .wm-search{position: relative; height: 100%; font-size: 14px;}
-.wm-search_title{line-height: 32px; padding: 8px 4px; background-image: linear-gradient(172deg, @Minor, #FFF); box-sizing: border-box;}
+.wm-search_title{line-height: 32px; padding: 4px; background-image: linear-gradient(172deg, @Minor, #FFF); box-sizing: border-box;}
 .wm-search_title h2{font-size: 14px; padding-left: 10px; font-weight: 600;}
 .wm-search_title span{cursor: pointer; width: 32px; height: 32px; line-height: 32px; text-align: center; border-radius: 50%;}
 .wm-search_title span i{font-size: 14px; color: @Info;}

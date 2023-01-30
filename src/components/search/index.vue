3f1b08d732e06dfb9ae8fd3f1f78fbda28555b19
webmis <klingsoul@163.com>
2023-01-30 08:55:07 +0800

    Search

--- src/components/search/index.vue
+++ src/components/search/index.vue
@@ -17,6 +17,8 @@
 
 <style lang="less">
 .wm-search{position: relative; height: 100%; font-size: 14px;}
+.wm-search li{position: relative;}
+.wm-search .line::before{content: ''; position: absolute; width: 100%; height: 1px; background-color: #E8E8E8;}
 .wm-search_title{line-height: 32px; padding: 4px; background-image: linear-gradient(172deg, @Minor, #FFF); box-sizing: border-box;}
 .wm-search_title h2{font-size: 14px; padding-left: 10px; font-weight: 600;}
 .wm-search_title span{cursor: pointer; width: 32px; height: 32px; line-height: 32px; text-align: center; border-radius: 50%;}

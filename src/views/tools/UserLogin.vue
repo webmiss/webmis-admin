261203efe323ff9242834b02c55fb15d02686764
webmis <klingsoul@163.com>
2025-06-04 10:08:46 +0800

    优化登录

--- src/views/tools/UserLogin.vue
+++ src/views/tools/UserLogin.vue
@@ -23,7 +23,9 @@
       <div class="wm-login_mask"></div>
       <div class="wm-login_body">
         <div class="wm-login_ct">
-          <div class="wm-login_logo" :style="{backgroundImage: login.uname&&login.uname==login.local_uname&&login.img?'url('+(login.img)+')':'', backgroundSize:login.uname&&login.uname==login.local_uname&&login.img?'100%':'60%'}"></div>
+          <div class="wm-login_logo" :style="{backgroundImage: login.uname&&login.uname==login.local_uname&&login.img?'url('+(login.img)+')':'', backgroundSize:login.uname&&login.uname==login.local_uname&&login.img?'100%':'60%'}">
+            <h2 class="uname" v-if="login.uname&&(login.is_passwd || login.is_safety)">{{ login.uname }}</h2>
+          </div>
           <div class="wm-login_form" v-if="!login.is_passwd">
             <i class="ui ui_user ico"></i>
             <input ref="loginUname" type="text" class="input" v-model="login.uname" @keyup.enter="clickLogin()" maxlength="32" :placeholder="state.langs.login_uname">
@@ -40,7 +42,7 @@
             <input ref="loginVcode" type="text" class="input" v-model="login.vcode" @keyup.enter="clickLogin()" maxlength="4" :placeholder="state.langs.login_vcode">
           </div>
           <div class="wm-login_bottom">
-            <span v-if="login.uname" class="flex">
+            <span v-if="login.uname">
               <i class="ui ui_arrow_left" @click="clearUser()"></i>
               <i class="ui ui_arrow_right" @click="clickLogin()"></i>
             </span>
@@ -79,17 +81,18 @@
 .wm-login_mask{position: absolute; z-index: 2; width: 100%; height: 100%; background: -webkit-radial-gradient(50% 30% ,farthest-corner,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.9));}
 .wm-login_body{position: absolute; z-index: 3; width: 100%; height: 100%; color: @Minor6;}
 .wm-login_ct{position: absolute; min-width: 300px; left: 50%; top: 50%; transform: translate(-50%, -60%);}
-.wm-login_logo{margin: 48px auto; width: 160px; height: 160px; border: rgba(0,0,0,0.8) 1px solid; background-color: rgba(0,0,0,0.7); background-image: url('../../assets/logo.svg'); border-radius: 50%; background-repeat: no-repeat; background-position: center; transition: All 0.5s ease-in-out;}
+.wm-login_logo{position: relative; margin: 56px auto; width: 160px; height: 160px; border: rgba(0,0,0,0.8) 1px solid; background-color: rgba(0,0,0,0.7); background-image: url('../../assets/logo.svg'); border-radius: 50%; background-repeat: no-repeat; background-position: center; transition: All 0.5s ease-in-out;}
 .wm-login_logo:hover{border-color: @Minor; background-color: rgba(0,0,0,0.7);}
+.wm-login_logo .uname{position: absolute; bottom: -45px; left: 50%; transform: translateX(-50%); font-size: 24px; text-shadow: 0 0 8px #000;}
 .wm-login_form{position: relative;}
 .wm-login_form input{width: 100%; padding: 0 16px 0 48px; height: 48px; line-height: 48px; color: @Minor8; font-size: 16px; border: transparent 1px solid; background-color: rgba(0,0,0,0.7); border-radius: 8px; box-sizing: border-box;}
 .wm-login_form input:hover{border-color: @Minor; background-color: rgba(0,0,0,0.8);}
 .wm-login_form input:focus{border-color: @Minor; background-color: rgba(0,0,0,0.8);}
 .wm-login_form .ico{position: absolute; width: 48px; line-height: 48px; color: @Minor5; font-size: 20px; text-align: center;}
 .wm-login_form .vcode{cursor: pointer; position: absolute; height: 40px; top: 50%; right: 4px; transform: translateY(-50%); border-radius: 4px;}
-.wm-login_bottom{text-align: center; line-height: 64px; text-shadow: 0 0 2px rgba(0,0,0,0.3);}
-.wm-login_bottom span{display: inline-block; line-height: 40px; padding: 0 16px; color: @Minor6;}
-.wm-login_bottom i{cursor: pointer; display: inline-block; width: 40px; margin: 0 8px; border-radius: 50%;}
+.wm-login_bottom{padding: 16px 0; line-height: 40px; text-shadow: 0 0 8px #000; display: flex; justify-content: center; align-items: center;}
+.wm-login_bottom span{display: flex; justify-content: center; align-items: center; padding: 0 16px; color: @Minor6;}
+.wm-login_bottom i{cursor: pointer; width: 40px; height: 40px; margin: 0 8px; border-radius: 50%; display: flex; justify-content: center; align-items: center;}
 .wm-login_bottom i:hover{background-color: rgba(0,0,0,0.8); color: @Minor;}
 </style>
 

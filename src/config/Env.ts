1cf5c9d55551bfc4eb151afe37aa1e5f8fe10290
webmis <klingsoul@163.com>
2025-10-29 18:09:23 +0800

    ERP-打包

--- src/config/Env.ts
+++ src/config/Env.ts
@@ -2,7 +2,7 @@
 export default class Env {
 
   public static mode: string = '';              // 模式: 开发(dev)
-  public static isSocket: boolean = false;       // Socket开启
+  public static isSocket: boolean = true;       // Socket开启
   public static title: string = 'WebMIS';       // 项目名称
   public static version: string = '3.0.0';      // 版本
   public static lang: string = 'zh_CN'          // 语言
